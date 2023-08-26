150dad0898315aed57946d76ac3ab22153b67c01
Jinxin Liu <43560642+Liu-Jinxin@users.noreply.github.com>
2023-08-26 21:21:01 +0800

    Add files via upload

---
+++ paper_chain/agent_chat.py
@@ -0,0 +1,30 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
+import yaml
+
+print('cuda_version:', torch.version.cuda)
+
+with open('/workspace/config.yaml', 'r') as file:
+    config = yaml.safe_load(file)
+hf_token = config['HUGGINGFACE_TOKEN']
+
+model = AutoModelForCausalLM.from_pretrained('meta-llama/Llama-2-7b-chat-hf', device_map='auto', torch_dtype=torch.float16, use_auth_token=hf_token, load_in_8bit=True)
+model =model.eval()
+tokenizer = AutoTokenizer.from_pretrained('meta-llama/Llama-2-7b-chat-hf', use_fast=False, use_auth_token=hf_token)
+tokenizer.pad_token = tokenizer.eos_token
+input_ids = tokenizer(['<s>Human: how to control robot arm?\n</s><s>Assistant: '], return_tensors="pt",add_special_tokens=False).input_ids.to('cuda')        
+generate_input = {
+    "input_ids":input_ids,
+    "max_new_tokens":512,
+    "do_sample":True,
+    "top_k":50,
+    "top_p":0.95,
+    "temperature":1,
+    "repetition_penalty":1.3,
+    "eos_token_id":tokenizer.eos_token_id,
+    "bos_token_id":tokenizer.bos_token_id,
+    "pad_token_id":tokenizer.pad_token_id
+}
+generate_ids  = model.generate(**generate_input)
+text = tokenizer.decode(generate_ids[0])
+print(text)
--- paper_chain/document_question_answering/test.py
+++ paper_chain/document_question_answering/test.py
@@ -1,24 +1,35 @@
+import yaml
+import os
 from transformers import AutoTokenizer
 import transformers
 import torch
 
+print(torch.version.cuda)
+
+with open('/home/jinxin/Desktop/github/paper_chain/paper_chain/config.yaml', 'r') as file:
+    config = yaml.safe_load(file)
+hf_token = config['HUGGINGFACE_TOKEN']
+
 model = "meta-llama/Llama-2-7b-chat-hf"
 
-tokenizer = AutoTokenizer.from_pretrained(model)
+tokenizer = AutoTokenizer.from_pretrained(model, use_auth_token=hf_token)
 pipeline = transformers.pipeline(
     "text-generation",
     model=model,
     torch_dtype=torch.float16,
-    device_map="auto",
+    device='cuda:0',
+    use_auth_token=hf_token
 )
 
 sequences = pipeline(
-    'I liked "Breaking Bad" and "Band of Brothers". Do you have any recommendations of other shows I might like?\n',
-    do_sample=True,
-    top_k=10,
+    'chunks',
+    top_k=50,
+    temperature=1,
     num_return_sequences=1,
+    top_p=0.95,
     eos_token_id=tokenizer.eos_token_id,
-    max_length=200,
+    max_length=1024,
+    max_new_tokens=1024
 )
 for seq in sequences:
     print(f"Result: {seq['generated_text']}")
